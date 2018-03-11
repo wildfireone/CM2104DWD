4ff4a6013814772980ab41b15f8cf9aa14fb2a11
john <wildfireone@gmail.com>
2018-03-11 18:56:56 +0000

    test

--- lab13/mythirdserver.js
+++ lab13/mythirdserver.js
@@ -10,7 +10,7 @@
 
  http.createServer(function (req, res) {
      res.writeHead(200, {'Content-Type': 'text/html'});
-      asciify('Awesome', function(err, textres){
+      asciify('Awesome',{font: 'larry3d', maxWidth: 90 }, function(err, textres){
         res.end(textres);
       });
  }).listen(8080);
