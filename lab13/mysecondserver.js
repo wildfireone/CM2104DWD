d77d970a08f0a49b337c70c607eb5bbc222791a2
john <wildfireone@gmail.com>
2018-03-11 18:19:53 +0000

    test

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -13,5 +13,5 @@ var currentdate = require('./mymodule');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently: " + currentdate.myDateTime());
-    res.end();
+    res.end('Hello World');
 }).listen(8080);
