540880438f6168f7b91f13a8afe8d49fcb8b7b71
john <wildfireone@gmail.com>
2018-03-11 18:06:50 +0000

    added second server

---
+++ lab13/mymodule.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: John Isaacs <john>
+ * @Date:   11-Mar-182018
+ * @Filename: mymodule.js
+ * @Last modified by:   john
+ * @Last modified time: 11-Mar-182018
+ */
+ exports.myDateTime = function () {
+     return Date();
+ };
---
+++ lab13/mysecondserver.js
@@ -0,0 +1,16 @@
+/**
+ * @Author: John Isaacs <john>
+ * @Date:   11-Mar-182018
+ * @Filename: myfirstserver.js
+ * @Last modified by:   john
+ * @Last modified time: 11-Mar-182018
+ */
+ var http = require('http');
+
+var currentdate = require('./mymodule');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+res.write(currentdate.myDateTime);
+    res.end('Hello World!');
+}).listen(8080);
