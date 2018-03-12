7b2db4eb07b8086a7fcbedea4629b7747f8a8f07
john <wildfireone@gmail.com>
2018-03-12 01:22:33 +0000

    tweets

--- lab14/public/main.js
+++ lab14/public/main.js
@@ -6,19 +6,25 @@
  * @Last modified time: 12-Mar-182018
  */
 
- $(function() {
-   // when page ready
-   //ask for json from our twitter JSON
-   $.getJSON( "/tweetsjson", function( data ) {
-     console.log(data);
+$(function() {
+  // when page ready
+  //ask for json from our twitter JSON
+  getTweets();
+//add interval code so this runs every 60 seconds
+  setInterval(getTweets, 60000);
+});
 
-     for(var t=0; t<data.length;t++){
-       var tweetoutput = "<div>";
-       tweetoutput +="<h2>"+data[i].name+"</h2>";
-       tweetoutput +="<p>"+data[i].text+"</p>";
-       tweetoutput +="</div>";
-       $('#tweetsdisplay').append(tweetoutput);
-     }
+function getTweets() {
+  $.getJSON("/tweetsjson", function(data) {
+    console.log(data);
 
-   });
- });
+    for (var t = 0; t < data.length; t++) {
+      var tweetoutput = "<div>";
+      tweetoutput += "<h2>" + data[i].name + "</h2>";
+      tweetoutput += "<p>" + data[i].text + "</p>";
+      tweetoutput += "</div>";
+      $('#tweetsdisplay').append(tweetoutput);
+    }
+
+  });
+}
--- lab14/server.js
+++ lab14/server.js
@@ -19,26 +19,7 @@ var client = new Twitter({
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-  var params = {
-    screen_name: 'nodejs'
-  };
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      var output = "";
-      for (var t = 0; t < tweets.length; t++) {
-        output += "<div>";
-        output += "<h2>" + tweets[t].user.screen_name + "<h2>";
-        output += "<p>" + tweets[t].text + "</p>"
-        output += "</div>";
-      }
-      res.send(output);
 
-    }
-  });
-
-
-});
 
 app.get('/tweetsjson', function(req, res) {
   var params = {
