2aeb3a7703d3df54977b1ce06e2c6776cc9e2192
john <wildfireone@gmail.com>
2018-03-18 19:06:27 +0000

    beautified

--- lab15/server.js
+++ lab15/server.js
@@ -5,29 +5,28 @@
  * @Last modified by:   john
  * @Last modified time: 18-Mar-182018
  */
- const MongoClient = require('mongodb').MongoClient;
- const url = "mongodb://localhost:27017/star_wars_quotes";
- const express = require('express');
- const app = express();
- app.use(express.static('public'));
- var db;
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/star_wars_quotes";
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+var db;
 
- MongoClient.connect(url, function(err, database){
-   if(err) throw err;
-   db = database;
-   app.listen(8080);
- });
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+});
 
 
 
- app.get('/', function(req, res){
-    db.collection('quotes').find().toArray(function(err, result){
-      var output ="";
-        for(var i=0; i<result.length;i++){
-          output+="<h2>"+result[i].name+"</h2>"
-          output+="<p>"+result[i].quote+"<p>"
-        }
-      res.send(output);
-    });
+app.get('/', function(req, res) {
+  db.collection('quotes').find().toArray(function(err, result) {
+    var output = "";
+    for (var i = 0; i < result.length; i++) {
+      output += "<h2>" + result[i].name + "</h2>"
+      output += "<p>" + result[i].quote + "<p>"
+    }
+    res.send(output);
   });
- });
+});
