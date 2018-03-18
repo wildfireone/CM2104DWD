5f846176b423b5c29ad1fdffc622eda03b1a8c9a
john <wildfireone@gmail.com>
2018-03-18 21:31:20 +0000

    test

--- lab15/server.js
+++ lab15/server.js
@@ -11,7 +11,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}))
