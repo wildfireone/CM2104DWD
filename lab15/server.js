93a5dcd2e4a2b60d5c3d5f2311738f392498862f
john <wildfireone@gmail.com>
2018-03-18 22:14:37 +0000

    test

--- lab15/server.js
+++ lab15/server.js
@@ -54,7 +54,7 @@ app.post('/search', function(req, res) {
   db.collection('quotes').find(req.body).toArray(function(err, result) {
     if (err) throw err;
 
-    var output = "<h1>All the quotes</h1>";
+    var output = "<h1>quotes by" +req.body.name+ "</h1>";
 
     for (var i = 0; i < result.length; i++) {
       output += "<div>"
@@ -63,5 +63,21 @@ app.post('/search', function(req, res) {
       output += "</div>"
     }
     res.send(output);
+  });
+});
+
+app.post('/delete', function(req, res) {
+  db.collection('quotes').deleteOne(req.body, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+app.post('/update', function(req, res) {
+  var query = { quote: req.body.quote };
+  var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
+  db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
   });
 });
