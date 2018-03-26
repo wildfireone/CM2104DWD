1821a695eea372554f6195965a1040288fa1597a
john <wildfireone@gmail.com>
2018-03-26 22:38:25 +0100

    added extra pages

--- lab18-profilesystem/server.js
+++ lab18-profilesystem/server.js
@@ -54,53 +54,13 @@ app.get('/profile', function(req, res) {
   });
 
 });
-app.get('/delete', function(req, res) {
-  res.render('pages/delete')
+app.get('/adduser', function(req, res) {
+  res.render('pages/adduser')
 });
-app.get('/update', function(req, res) {
-  res.render('pages/update')
-});
-
-
-
-app.get('/allquotes', function(req, res) {
-  db.collection('quotes').find().toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    var output = "<h1>All the quotes</h1>";
-    for (var i = 0; i < result.length; i++) {
-      output += "<div>"
-      output += "<h3>" + result[i].name + "</h3>"
-      output += "<p>" + result[i].quote + "</p>"
-      output += "</div>"
-    }
-    res.send(output);
-  });
+app.get('/remuser', function(req, res) {
+  res.render('pages/remuser')
 });
 
-app.post('/quotes', function(req, res) {
-  db.collection('quotes').save(req.body, function(err, result) {
-    if (err) throw err;
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.post('/search', function(req, res) {
-  db.collection('quotes').find(req.body).toArray(function(err, result) {
-    if (err) throw err;
-
-    var output = "<h1>quotes by" + req.body.name + "</h1>";
-
-    for (var i = 0; i < result.length; i++) {
-      output += "<div>"
-      output += "<h3>" + result[i].name + "</h3>"
-      output += "<p>" + result[i].quote + "</p>"
-      output += "</div>"
-    }
-    res.send(output);
-  });
-});
 
 app.post('/delete', function(req, res) {
   db.collection('quotes').deleteOne(req.body, function(err, result) {
